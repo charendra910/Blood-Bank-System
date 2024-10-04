2871d0d376cb939b7f00985a67f867a1582e30bf
charendra910 <charendra910@rku.ac.in>
2024-10-04 22:12:05 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blood_Bank_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
