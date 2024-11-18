b68cfb40674f806f54bbd60a38e2a1a388446145
Harendra Chaudhary <119689787+charendra910@users.noreply.github.com>
2024-11-18 18:03:52 +0530

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,12 @@
 namespace Blood_Bank_System.Models
 {
     public class ErrorViewModel
+        
     {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        
     }
 }
